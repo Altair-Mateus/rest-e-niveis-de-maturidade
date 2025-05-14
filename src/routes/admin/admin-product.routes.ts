33198f08192d8e3a465a9be056d0a139a6abfa21
Altair-Mateus <taicoalencastro@gmail.com>
2025-05-13 21:04:27 -0300

    Aplicado o nivel 1 de maturidade de Richardson

---
+++ src/routes/admin/admin-product.routes.ts
@@ -0,0 +1,94 @@
+import { Router } from "express";
+import { createProductService } from "../../services/product.service";
+
+const router = Router();
+
+router.post("/", async (req, res) => {
+  const productService = await createProductService();
+  const { name, slug, description, price, categoryIds } = req.body;
+  const product = await productService.createProduct(
+    name,
+    slug,
+    description,
+    price,
+    categoryIds
+  );
+  res.json(product);
+});
+
+router.get("/:productId", async (req, res) => {
+  const productService = await createProductService();
+  const product = await productService.getProductById(+req.params.productId);
+  res.json(product);
+});
+
+router.post("/:productId", async (req, res) => {
+  const productService = await createProductService();
+  const { name, slug, description, price, categoryIds } = req.body;
+  const product = await productService.updateProduct({
+    id: +req.params.productId,
+    name,
+    slug,
+    description,
+    price,
+    categoryIds,
+  });
+  res.json(product);
+});
+
+router.post("/:productId/delete", async (req, res) => {
+  const productService = await createProductService();
+  await productService.deleteProduct(+req.params.productId);
+  res.send({ message: "Product deleted successfully" });
+});
+
+router.get("/", async (req, res) => {
+  const productService = await createProductService();
+  const {
+    page = 1,
+    limit = 10,
+    name,
+    categories_slug: categoriesSlugStr,
+  } = req.query;
+  const categories_slug = categoriesSlugStr
+    ? categoriesSlugStr.toString().split(",")
+    : [];
+  const { products, total } = await productService.listProducts({
+    page: parseInt(page as string),
+    limit: parseInt(limit as string),
+    filter: {
+      name: name as string,
+      categories_slug,
+    },
+  });
+  res.json({ products, total });
+});
+
+router.get("/listProducts.csv", async (req, res) => {
+  const productService = await createProductService();
+  const {
+    page = 1,
+    limit = 10,
+    name,
+    categories_slug: categoriesSlugStr,
+  } = req.query;
+  const categories_slug = categoriesSlugStr
+    ? categoriesSlugStr.toString().split(",")
+    : [];
+  const { products } = await productService.listProducts({
+    page: parseInt(page as string),
+    limit: parseInt(limit as string),
+    filter: {
+      name: name as string,
+      categories_slug,
+    },
+  });
+  const csv = products
+    .map((product) => {
+      return `${product.name},${product.slug},${product.description},${product.price}`;
+    })
+    .join("\n");
+  res.send(csv);
+});
+
+export default router;
